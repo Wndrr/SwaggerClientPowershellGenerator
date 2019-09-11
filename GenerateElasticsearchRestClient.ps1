3497151daa04ca2d1b33759e4280db51c42e4f43
Wndrr <mathieu@viales.fr>
2019-09-11 14:39:53 +0200

    Update and rename GenerateElasticsearchRestClient.ps1 to ExampleExecution.ps1

--- GenerateElasticsearchRestClient.ps1
+++
@@ -1,8 +0,0 @@
-. "C:\Repos\Work\Portails\Tools\Elasticsearch\RegenElasticsearchClient.ps1"
-
-$SwaggerGenerator = [SwaggerClientGenerator]::new("../../ElasticsearchClient", "http://localhost:56312/swagger/docs/v1", "swagger-codegen-cli.jar");
-
-$SwaggerGenerator.Generate();
-
-Write-Host -NoNewLine 'Press any key to exit';
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
